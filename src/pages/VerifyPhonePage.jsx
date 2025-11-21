f1a627b0f423ecaa392f9c4cc9fb958966e4ebf4
sofiene-feki <sofian.feki@gmail.com>
2025-11-21 16:11:22 +0100

    button

--- src/pages/VerifyPhonePage.jsx
+++ src/pages/VerifyPhonePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useFirebaseAuth } from "@/hooks/useFirebaseAuth";
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 
 export default function VerifyPhonePage() {
   const navigate = useNavigate();
